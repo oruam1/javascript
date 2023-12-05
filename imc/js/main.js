b0e61127896d0e83441c25f4c92413c55ff92cae
mauro de mello <mauro.m.junior@aluno.senai.br>
2023-12-05 19:34:29 -0300

    aula 05 12

--- imc/js/main.js
+++ imc/js/main.js
@@ -1,47 +1,48 @@
 const calcularImc = () => {
-    const nome = 
+    const nome =
         document.getElementById("nome").value;
-    const altura = 
+    const altura =
         document.getElementById("altura").value;
-    const peso = 
+    const peso =
         document.getElementById("peso").value;
-    const resultado = 
+    const resultado =
         document.getElementById("resultado");
 
-    if( nome !== '' && peso !== '' && altura !== ''){
-        const imc = (peso/(altura*altura));
+    if (nome !== '' && peso !== '' && altura !== '') {
+        const imc = (peso / (altura * altura)).toFixed(2);
         //comando para exibir no console 'f12'
         console.log('altura: ', altura);
 
         let classificacao = '';
 
-        if(imc < 18.5){
+        if (imc < 18.5) {
             classificacao = 'Abaixo do peso';
         }
-        else if(imc < 25){
+        else if (imc < 25) {
             classificacao = 'compeso ideal. parabens!!!';
         }
-        else if (imc < 30){
+        else if (imc < 30) {
             classificacao = 'levemente acima do peso.';
         }
-        else if (imc < 35){
+        else if (imc < 35) {
             classificacao = 'obesidade grau I';
         }
-        else if(imc < 40){
+        else if (imc < 40) {
             classificacao = 'obesidade grau II';
         }
         else {
             classificacao = 'obesidade grau III. cuidado!!!';
         }
-        resultado.textContent = 
+        resultado.textContent =
             `${nome}, seu IMC e ${imc} e vc esta ${classificacao}`
 
     }
     else {
-        resultado.textContent = 
-        'preencha todos os campos';
+        resultado.textContent =
+            'preencha todos os campos';
     }
-    
+
+    limparCampos()
 };
 const limparCampos = () => {
     const nome =
@@ -50,5 +51,20 @@ const limparCampos = () => {
         document.getElementById("altura").value = null;
     const peso =
         document.getElementById("peso").value = null;
-
+    ;
 }
+
+const mascaraAltura = (value, pattern) => {
+    let i = 0;
+    let v = value.toString();
+    v = v.replace(/\D/g, "");
+    return pattern.replace(/#/g, () => v[i++] || "");
+}
+const aplicar = (value) => {
+    const formatado = mascaraAltura(value, '#.##');
+    document.getElementById('altura').value = formatado;
+};
+
+
+
+
