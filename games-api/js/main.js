c7621997824444075a1e112d77e3658e8282bab5
mauro de mello <mauro.m.junior@aluno.senai.br>
2023-12-12 21:28:59 -0300

    refatoração

--- games-api/js/main.js
+++ games-api/js/main.js
@@ -1,8 +1,8 @@
-import { getAllGamers } from "./service";
+import { getAllGamers, deleteGame } from "./service";
 window.onload = () => {
     loadGames();
 };
-
+//refatorado
 const loadGames = () => {
     const dataContainer =
         document.getElementaById('data-container');
@@ -12,56 +12,40 @@ const loadGames = () => {
                 document.createElement('div');
             gamesElement.innerHTML =
                 `<strong>${game.nome}</strong><p>${game.preco}</p>`;
-                dataContainer.appendChild(gamesElement);
+            dataContainer.appendChild(gamesElement);
         });
     })
 }
-const createGame = () => {
-    const testGame = {
-        "nome": "The legend of Zelda",
-        "img": "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
-        "preco": 300
-    };
+document.getElementById = ('btnCreate')
+    .addEventeListerner('click', () => {
+        const jogo = {
+            "nome": "The legend of Zelda 2",
+            "img": "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
+            "preco": 3000
+        };
+        createGame(jogo);
+    });
+Document.getElementById('btnDelete')
+    .addEventeListerner('click', () => {
+        const jogo = {
+            "nome": "The legend of Zelda 2",
+            "img": "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
+            "preco": 300,
+            "id": 3
+        };
+        deleteGame(jogo);
+    });
 
-    fetch(URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(testGame)
-    })
-        .then(response => response.json())
-        .then(data => console.log('success: ', data))
-        .catch((error) => console.log('Error: ', error));
-};
 
-const deleteGame = () => {
-    const game = {
-        "id": 3
-    };
+document.getElementById('btnUpdate')
+    .addEventeListerner('click', () => {
+        const jogo = {
+            nome: "The legend of zelda 2",
+            img: "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
+            preco: 200,
+            id: 3
+        };
+        updateGame(jogo)
+    });
 
-    fetch(URL + `/${game.id}`, { method: 'DELETE' })
-        .then(response => response.json())
-        .then(data => console.log('Success:', data))
-        .catch(error => console.error('Error:', error));
-};
 
-const updateGame = () => {
-    const game = {
-        nome: "zelda 2",
-        img: "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
-        preco: 200,
-        id: 3
-    };
-
-    fetch(URL + `/${game.id}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'Application/json'
-        },
-        body: JSON.stringify(game)
-    })
-        .then(response => response.json())
-        .then(data => console.log('success: ', data))
-        .catch((error) => console.log('Error: ', error));
-};
--- games-api/js/service.js
+++ games-api/js/service.js
@@ -15,3 +15,35 @@ export const getAllGames = () => {
         console.log('Error >>>', error);
     }
 };
+export const createGame = async (game) => {
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(testGame)
+    })
+        .then(response => response.json())
+        .then(data => console.log('success: ', data))
+        .catch((error) => console.log('Error: ', error));
+};
+export const deleteGame = async (game) => {
+}
+export const deleteGame =async (game) => {
+    fetch(URL + `/${game.id}`, { method: 'DELETE' })
+        .then(response => response.json())
+        .then(data => console.log('Success:', data))
+        .catch(error => console.error('Error:', error));
+};
+export const updateGame = async (game) => {
+    fetch(URL + `/${game.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'Application/json'
+        },
+        body: JSON.stringify(game)
+    })
+        .then(response => response.json())
+        .then(data => console.log('success: ', data))
+        .catch((error) => console.log('Error: ', error));
+}
